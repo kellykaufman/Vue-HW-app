90cfe420100566e6168c3b1dc75aff9d51e90a14
Kelly Kaufman <kellykaufman12@gmail.com>
2021-11-10 17:01:29 -0800

    fixes data attribute function

--- index.js
+++ index.js
@@ -2,6 +2,17 @@
 var app = new Vue({
   el: "#app",
   data: function () {
-    message: "Hello from JavaScript!";
-  },
+    return {
+    message: "Movie list below!",
+    movies: [
+      "Titane",
+      "Monster's Ball",
+      "Queen & Slim",
+      "Waves",
+      "Jacob's Ladder",
+      "Hunchback of Notre Dame",
+    ],
+  }
 });
+
+// <!-- THis is as far as I could get. I have a limited amount of time to practice code outside class, and need to use the rest of my time to catch up/understand the class exercises. -->
